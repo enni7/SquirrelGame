0f73d23a3be8b976cf14f10fb0b712c576033ce2
enni <annaizzo7@hotmail.it>
2022-03-29 19:32:57 +0200

    first commit

---
+++ SquirrelGame/SquirrelGame/SquirrelGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquirrelGameApp.swift
+//  SquirrelGame
+//
+//  Created by Anna Izzo on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SquirrelGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
